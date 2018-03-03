552e06c3f29959ae45726b03cd9d5c7c69b378ff
Benjamin Zekavica <info@benjamin-zekavica.de>
2018-03-03 16:58:59 +0100

    Add ninja forms DE Lang

--- js/func.js
+++ js/func.js
@@ -34,4 +34,32 @@ jQuery(document).ready(function() {
 		var ausleser = jQuery('.name strong').html();
 		jQuery('input.mks-vorname').val(ausleser);
 	}); 
-	 
+	
+	
+	
+/* Location DE - German String Translation */
+
+jQuery(document).ready( function() {
+		
+			var customDatePickerStuff = Marionette.Object.extend( {
+			initialize: function() {
+				this.listenTo( Backbone.Radio.channel( 'pikaday' ), 'init', this.modifyDatepicker );	
+			},
+
+			// Edit Lables 
+			modifyDatepicker: function( dateObject, fieldModel ) {
+				
+				 dateObject.pikaday._o.i18n = {
+					previousMonth : 'vor',
+					nextMonth     : 'zurück',
+					months        : ['Januar','Februar','März','April','Mai','Juni','Juli','August','Septemper','Oktober','November','Dezember'],
+					weekdays      : ['Sonntag','Montag','Dienstag','Mittwoch','Donnerstag','Freitag','Samstag'],
+					weekdaysShort : ['So','Mo','Di','Mi','Do','Fr','Sa']
+				};
+			}
+	    });
+
+		jQuery( document ).ready( function() {
+			new customDatePickerStuff();
+		} );
+}); 
